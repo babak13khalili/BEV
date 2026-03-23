c7c09daf926e786e16d808a12222166b8d233486
babak <babakkhalili1303@gmail.com>
2026-03-23 18:48:07 +0100

    Firebase-connection

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,8 @@
-window.BEV_FIREBASE_CONFIG = null;
-
-/*
-Paste your Firebase web app config here, for example:
-
 window.BEV_FIREBASE_CONFIG = {
-  apiKey: "AIza...",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  storageBucket: "your-project.firebasestorage.app",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  apiKey: "AIzaSyDYVUmmCS8KE2XD5JNhh1bkFurKoaGATB4",
+  authDomain: "birdeyeview-609cc.firebaseapp.com",
+  projectId: "birdeyeview-609cc",
+  storageBucket: "birdeyeview-609cc.firebasestorage.app",
+  messagingSenderId: "437382283451",
+  appId: "1:437382283451:web:08c679a9ce93fa4abd699e",
 };
-*/
